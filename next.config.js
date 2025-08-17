8ed498f0f9fbc57893c98b93a8dd8e08e63c2339
Robert Schwentker <schwentker@gmail.com>
2025-08-17 16:32:42 -0700

    Add basePath for autonitylend subdirectory

--- next.config.js
+++ next.config.js
@@ -2,12 +2,13 @@
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: '/autonitylend',
   images: {
     unoptimized: true
   },
   typescript: {
     ignoreBuildErrors: true,
-  },
+  }
 }
 
 module.exports = nextConfig
